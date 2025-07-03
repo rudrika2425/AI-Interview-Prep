8687792bf11b92c8f37fd6ad3a32d5cbc60e203b
Rudrika Raghav <116345691+rudrika2425@users.noreply.github.com>
2025-07-03 10:42:05 +0530

    name change

--- Frontend/src/pages/Auth.jsx
+++ Frontend/src/pages/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../context/userContext";
+import { UserContext } from "../context/UserContext";
 
 const Auth = ({ onClose }) => {
   const {login} = useContext(UserContext);
